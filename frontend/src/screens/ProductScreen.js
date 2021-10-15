974041070bc65a6aa6909794237e245793fcda16
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-10-15 09:37:09 +0100

    frontend:Product Details Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,74 @@
 import React from 'react';
 import { useParams } from 'react-router';
-
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
 import products from '../products.js';
+
 const ProductScreen = () => {
 	let { id } = useParams();
 
-	const product = products.filter((product) => product._id == id);
+	const product = products.find((p) => p._id === id);
 
 	return (
-		<div>
-			<h1>Product Name: {product[0].name}</h1>
-		</div>
+		<>
+			<Link className="btn btn-dark my-3" to="/">
+				<i className="fas fa-arrow-left"></i> Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image src={product.image} alt={product.name} fluid />
+				</Col>
+				<Col md={3}>
+					<ListGroup variant="flush">
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={3}>
+					<Card>
+						<ListGroup>
+							<ListGroup.Item>
+								<Row>
+									<Col>Price:</Col>
+									<Col>
+										<strong>${product.price}</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Status:</Col>
+									<Col>
+										{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Button
+										className="btn-block"
+										type="button"
+										disabled={product.countInStock === 0}
+									>
+										Add To Cart
+									</Button>
+								</Row>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
 	);
 };
 
