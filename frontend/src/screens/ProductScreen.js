a8eea790343be222b9568582a3d59daed4d4dc1b
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-10-19 05:43:30 +0100

    frontend:Adding quantity seect to product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form,
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Rating from '../components/Rating';
 import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ match, history }) => {
+	const [qty, setQty] = useState(0);
 	const dispatch = useDispatch();
 
 	useEffect(async () => {
@@ -17,9 +26,12 @@ const ProductScreen = ({ match }) => {
 	const productDetails = useSelector((state) => state.productDetails);
 	const { loading, error, product } = productDetails;
 
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`);
+	};
 	return (
 		<>
-			<Link className="btn btn-dark my-3" to="/">
+			<Link className="btn btn-light my-3" to="/">
 				<i className="fas fa-arrow-left"></i> Go Back
 			</Link>
 			{loading ? (
@@ -67,12 +79,36 @@ const ProductScreen = ({ match }) => {
 										</Col>
 									</Row>
 								</ListGroup.Item>
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as="select"
+													size="sm"
+													value={qty}
+													onChange={(e) => setQty(e.target.value)}
+												>
+													{[...Array(product.countInStock).keys()].map((x) => {
+														return (
+															<option key={x + 1} value={x + 1}>
+																{x + 1}
+															</option>
+														);
+													})}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 								<ListGroup.Item>
 									<Row>
 										<Button
 											className="btn-block"
 											type="button"
 											disabled={product.countInStock === 0}
+											onClick={addToCartHandler}
 										>
 											Add To Cart
 										</Button>
