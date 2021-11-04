21f99ecc893bf79d562a74f637507eecf385c858
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-11-04 04:07:20 +0100

    Cart reducer & actions

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -16,5 +16,6 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 		},
 	});
 
+	console.log(JSON.stringify(getState().cart.cartItems));
 	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,11 +18,7 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
 	: [];
 
-const initialState = {
-	cart: {
-		cartItems: cartItemsFromStorage,
-	},
-};
+const initialState = {};
 
 const middleware = [thunk];
 
