f885cd8923041dbc397d5f904b596701ec835e1a
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-11-05 08:08:53 +0100

    Profile Screen & Get User Details

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -30,12 +30,7 @@ const Header = () => {
 								</Nav.Link>
 							</LinkContainer>
 							{userInfo ? (
-								<NavDropdown
-									bg="dark"
-									variant="dark"
-									title={userInfo.name}
-									id="username"
-								>
+								<NavDropdown title={userInfo.name} id="username">
 									<LinkContainer to="/profile">
 										<NavDropdown.Item>Profile</NavDropdown.Item>
 									</LinkContainer>
