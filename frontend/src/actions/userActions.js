d30787ea0b8bfb9df4fd78a7082cd6d05ed5fc50
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-11-05 07:12:36 +0100

    add show user in navbar & Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,32 +5,35 @@ import {
 	USER_LOGIN_FAIL,
 	USER_LOGOUT,
 } from '../constants/userConstants.js';
-export const userLogin =
-	({ email, password }) =>
-	async (dispatch) => {
-		try {
-			dispatch({ type: USER_LOGIN_REQUEST });
+export const login = (email, password) => async (dispatch) => {
+	try {
+		dispatch({ type: USER_LOGIN_REQUEST });
 
-			const config = {
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			};
-			const { data } = await axios.post(
-				'/api/users/login',
-				{ email, password },
-				config
-			);
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		const { data } = await axios.post(
+			'/api/users/login',
+			{ email, password },
+			config
+		);
 
-			dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-			localStorage.setItem('userInfo', JSON.stringify(data));
-		} catch (error) {
-			dispatch({
-				type: USER_LOGIN_FAIL,
-				payload:
-					error.response && error.response.data.message
-						? error.response.data.message
-						: error.message,
-			});
-		}
-	};
+		dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+		localStorage.setItem('userInfo', JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: USER_LOGIN_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export const logout = () => async (dispatch) => {
+	localStorage.removeItem('userInfo');
+	dispatch({ type: USER_LOGOUT });
+};
