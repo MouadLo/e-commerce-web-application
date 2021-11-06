d4ade819221c7954f86e6d5f65c245fec33578e3
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-11-06 08:03:07 +0100

    Prepare for depoloyment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-
+import path from 'path';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
@@ -16,13 +16,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
-
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, 'frontend', 'build', 'index.html')
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
