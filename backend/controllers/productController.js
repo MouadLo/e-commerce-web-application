5c45f545f9ff61752f889bd9218f401136860513
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-11-09 01:30:41 +0100

    Add create review endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import products from '../data/products.js';
 import Product from '../models/productModel.js';
 
 // @desc Fetch all products
@@ -85,10 +86,52 @@ const updateProduct = asyncHandler(async (req, res) => {
 	}
 });
 
+// @desc Create a new review
+// @route POST /api/products/:id/review
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+	const { rating, comment } = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		const alreadyReviewd = product.reviews.find(
+			(r) => r.user.toString() === req.user._id.toString()
+		);
+
+		if (alreadyReviewd) {
+			res.status(404);
+			throw new Error('Product already reviewed');
+		}
+
+		const review = {
+			name: req.user.name,
+			rating: Number(rating),
+			comment,
+			user: req.user._id,
+		};
+
+		product.reviews.push(review);
+
+		product.numReviews = product.reviews.length;
+
+		product.rating =
+			product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+			product.reviews.length;
+
+		await product.save();
+		res.status(201).json({ message: 'Review added' });
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
+
 export {
 	getProductById,
 	getProducts,
 	deleteProduct,
 	createProduct,
 	updateProduct,
+	createProductReview,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
 	createProduct,
+	createProductReview,
 	deleteProduct,
 	getProductById,
 	getProducts,
@@ -15,5 +16,7 @@ router
 	.get(getProductById)
 	.delete(protect, isAdmin, deleteProduct)
 	.put(protect, isAdmin, updateProduct);
+
+router.route('/:id/review').post(protect, createProductReview);
 
 export default router;
